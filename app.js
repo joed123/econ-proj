755a1d42561cff9316b59613534c93363e56d75f
rowanmorkner <56097720+rowanmorkner@users.noreply.github.com>
2023-10-13 17:26:10 -0700

    now its not working but ill update it anyways

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const data = [5.56, 5.60, 5.62, 5.62, 5.58, 5.49, 5.12, 4.88, 4.72, 4.73, 4.69, 5.00, 4.81];
-    const minValue = Math.min(...data); // Calculate the minimum value
+    let chart; 
 
-    Highcharts.chart('container', {
-        title: {
-            text: 'Yield Curve',
-        },
-        subtitle: {
-            text: 'Data from: 10/02/2023',
-        },
-        xAxis: {
-            categories: ['1 Mo', '2 Mo', '3 Mo', '4 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr', '7 Yr', '10 Yr', '20 Yr', '30 Yr'],
-            title: {
-                text: 'Time'
-            }
-        },
-        yAxis: {
-            title: {
-                text: 'Yield (%)'
-            },
-            min: minValue // Use the calculated minimum value
-        },
-        series: [{
-            name: 'Yield',
-            data: data
-        }]
-    });
+    document.getElementById('selectedDate').addEventListener('change', displayData);
+
+    function displayData() {
+        const selectedDate = document.getElementById('selectedDate').value;
+
+        // Find data for the selected date
+        const dataForRow = allData.find(row => row.Date === selectedDate);
+
+        if (!dataForRow) {
+            alert('No data available for the selected date!');
+            return;
+        }
+
+        const yieldData = Object.values(dataForRow).slice(1); // Extracting yield values
+        const minValue = Math.min(...yieldData); // Calculating the minimum value
+        
+        if (chart) {
+            // Update series, subtitle, and yAxis if the chart exists
+            chart.series[0].setData(yieldData);
+            chart.setTitle(null, {text: 'Data for: ' + selectedDate});
+            chart.yAxis[0].update({min: minValue});
+        } else {
+            // Create chart if it doesn't exist
+            chart = Highcharts.chart('container', {
+                title: {
+                    text: 'Yield Curve',
+                },
+                subtitle: {
+                    text: 'Data for: ' + selectedDate,
+                },
+                xAxis: {
+                    categories: ['1 Mo', '2 Mo', '3 Mo', '4 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr', '7 Yr', '10 Yr', '20 Yr', '30 Yr'],
+                    title: {
+                        text: 'Maturity'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: 'Yield (%)'
+                    },
+                    min: minValue // Use calculated minimum value
+                },
+                series: [{
+                    name: 'Yield',
+                    data: yieldData
+                }]
+            });
+        }
+    }
+    // You may call displayData once at the end of your DOMContentLoaded callback to render the initial chart.
+    displayData();
 });
