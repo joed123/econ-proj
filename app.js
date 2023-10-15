180edd9d2ed822e9f1fcdcafc5dd05eb4e7fc0b7
rowanmorkner <56097720+rowanmorkner@users.noreply.github.com>
2023-10-15 16:14:01 -0700

    again no worky but i think im getting close

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
             type: 'spline'
         },
         data: {
-            // enablePolling: true,
-            csvURL: window.location.origin + '/test.csv'
+            csvURL: window.location.origin + '/test.csv',
         },
         title: {
             text: 'US Treasury Yields'
@@ -16,52 +15,13 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         },
         xAxis: {
-            categories: ['1 Mo', '2 Mo', '3 Mo', '4 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr', '7 Yr', '10 Yr', '20 Yr', '30 Yr'],
+            type: 'catagories',
             title: {
-                text: 'Maturity'
+                text: 'maturity'
             }
         },
-        // Your remaining options here...
     });
 });
 
 
-
-
-
-
-
-    // const options = {
-    //      chart: {
-    //         type: 'spline',
-    //         zoomType: 'xy'
-    //     },
-    //     title: {
-    //         text: 'Yield Curve',
-    //     },
-    //     subtitle: {
-    //         text: 'Data for: selectedDate',
-    //         },
-        // xAxis: {
-        //     categories: ['1 Mo', '2 Mo', '3 Mo', '4 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr', '7 Yr', '10 Yr', '20 Yr', '30 Yr'],
-        //     title: {
-        //     text: 'Maturity'
-        //     }
-        //     },
-    //     yAxis: {
-    //         title: {
-    //         text: 'Yield (%)'
-    //         },
-    //         /*min: minValue // Use calculated minimum value*/
-    //         }, 
-    //     };
-    
-    // $.get('test.csv', csv =>{
-    //     options.data = {
-    //         csv
-    //     };
-    //     highcharts.chart('container', options);
-    // });
-    // You may call displayData once at the end of your DOMContentLoaded callback to render the initial chart.
-   
 
